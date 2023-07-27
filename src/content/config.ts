bde96d9e9e708bd350f0ee42f0429393c6547512
eomchanu <djacksdn1@icloud.com>
2023-07-27 20:55:57 +0900

    Posted test problem - boj1891

--- src/content/config.ts
+++ src/content/config.ts
@@ -22,13 +22,29 @@ const storeSchema = z.object({
     heroImage: z.string().optional(),
 });
 
+const problemsSchema = z.object({
+    title: z.string(),
+    description: z.string(),
+    custom_link_label: z.string(),
+    custom_link: z.string().optional(),
+    updatedDate: z.coerce.date(),
+    pricing: z.string().optional(),
+    oldPricing:  z.string().optional(),
+    badge: z.string().optional(),
+    checkoutUrl: z.string().optional(),
+    heroImage: z.string().optional(),
+});
+
 export type BlogSchema = z.infer<typeof blogSchema>;
 export type StoreSchema = z.infer<typeof storeSchema>;
+export type ProblemsSchema = z.infer<typeof problemsSchema>;
 
 const blogCollection = defineCollection({ schema: blogSchema });
 const storeCollection = defineCollection({ schema: storeSchema });
+const problemCollection = defineCollection({ schema: problemsSchema });
 
 export const collections = {
     'blog': blogCollection,
-    'store': storeCollection
+    'store': storeCollection,
+    'problems': problemCollection
 }
